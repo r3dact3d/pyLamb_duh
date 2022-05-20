317673bc8c7df0de1c3e9b22c449bdf783779ff0
Brady Thompson <r3dact3d@users.noreply.github.com>
2022-05-20 11:24:27 -0500

    Create aprsfiBasic.py

---
+++ aprsfiBasic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# written by brady [r3dact3d]
+import requests
+from sys import argv
+
+script, apiKey = argv
+
+payload = {
+'name': 'wd4rnr-9',
+'what': 'loc',
+'apikey': apiKey,
+'format': 'json'
+}
+
+url = 'https://api.aprs.fi/api/get'
+request = requests.get(url, params=payload)
+
+if request:
+    print(request)
+else:
+    print('An error has occured with the request.')
